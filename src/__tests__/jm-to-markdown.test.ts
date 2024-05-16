640eb622c443a613da726e526ea166b62467b319
Charlie Greenman <charlie@razroo.com>
2024-05-16 18:19:50 -0400

    ZETA-8747: update unit test for razroo

--- src/__tests__/jm-to-markdown.test.ts
+++ src/__tests__/jm-to-markdown.test.ts
@@ -21,24 +21,46 @@ expect(result).toEqual(expected);
 });
 
 it('should convert jira markdown with special curly braces to markdown', () => {
-  const jiraMarkdown = "{noformat}Scenario We are not sure whether or not scheduling via close is a users local time on their computer or via timezone in close.com system\n  Given I am Henry \n    And I am acting on behalf of Charlie Greenman\n  When I use Charlie Greenman's Close.com account \n    And Send out an email via send later\n  Then It should send an email at the expected time{noformat}\n\nNotes: \n\n# I will send an email to my email address 10 minutes later than the current time to test. Feel free to use this method to test. Once receive email can mark this ticket as complete"
+  const jiraMarkdown = "{noformat}Scenario Charlie Greenman would like to apply to 20 remote Angular contract roles\n  Given It is a LinkedIn job post\n    And It is remote\n    And It is a contract job\n  When it is Easy Apply\n  Then I should apply to it\n    And should use the existing resume{noformat}\n\nNotes\n\n# For now we will only apply to roles if they are remote, a contract and angular roles.\n## Can use this URL [https://www.linkedin.com|https://www.linkedin.com]\n# If asks for a cover letter, no need to supply one(just the resume)\n## Charlie Greenman’s resume if need it [https://drive.google.com|https://drive.google.com|smart-link] "
   const result = convertJiraMarkdownToRegularMarkdown(jiraMarkdown);
-  const expected = `\`\`\`Scenario We are not sure whether or not scheduling via close is a users local time on their computer or via timezone in close.com system
-  Given I am Henry 
-    And I am acting on behalf of Charlie Greenman
-  When I use Charlie Greenman's Close.com account 
-    And Send out an email via send later
-  Then It should send an email at the expected time\`\`\`
+  const expected = `\`\`\`Scenario Charlie Greenman would like to apply to 20 remote Angular contract roles
+  Given It is a LinkedIn job post
+    And It is remote
+    And It is a contract job
+  When it is Easy Apply
+  Then I should apply to it
+    And should use the existing resume\`\`\`
 
-Notes: 
+Notes
 
-1. I will send an email to my email address 10 minutes later than the current time to test. Feel free to use this method to test. Once receive email can mark this ticket as complete`;
+1. For now we will only apply to roles if they are remote, a contract and angular roles.
+   1. Can use this URL [https://www.linkedin.com](https://www.linkedin.com)
+1. If asks for a cover letter, no need to supply one(just the resume)
+   1. Charlie Greenman’s resume if need it [https://drive.google.com](https://drive.google.com) `;
     expect(result).toEqual(expected);
   });
 });
 
 describe('convertJiraMarkdownToHtml', () => {
   it('should convert jira markdown to html', async() => {
+    const jiraMarkdown = "*Feature*: Convert Jira ticket ADF description to HTML\n\nScenario: User converts Jira ticket ADF to HTML\nGiven that I have a Jira ticket ADF description\nWhen I initiate the conversion process\n  And use the importJiraTicket function\nThen the ADF description is successfully converted to HTML\n\nNotes: \n\n# Will use the function here: [https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml|https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml|smart-link] \n# Will be within the importJiraTickets function";
+    const result = await convertJiraMarkdownToHtml(jiraMarkdown);
+    const expected = `<p><strong>Feature</strong>: Convert Jira ticket ADF description to HTML</p>
+<p>Scenario: User converts Jira ticket ADF to HTML
+Given that I have a Jira ticket ADF description
+When I initiate the conversion process
+  And use the importJiraTicket function
+Then the ADF description is successfully converted to HTML</p>
+<p>Notes: </p>
+<ol>
+<li>Will use the function here: <a href="https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml">https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml</a> </li>
+<li>Will be within the importJiraTickets function</li>
+</ol>
+`;
+    expect(result).toEqual(expected);
+  });
+
+  it('should convert jira markdown to html if contains code as well', async() => {
     const jiraMarkdown = "*Feature*: Convert Jira ticket ADF description to HTML\n\nScenario: User converts Jira ticket ADF to HTML\nGiven that I have a Jira ticket ADF description\nWhen I initiate the conversion process\n  And use the importJiraTicket function\nThen the ADF description is successfully converted to HTML\n\nNotes: \n\n# Will use the function here: [https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml|https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml|smart-link] \n# Will be within the importJiraTickets function";
     const result = await convertJiraMarkdownToHtml(jiraMarkdown);
     const expected = `<p><strong>Feature</strong>: Convert Jira ticket ADF description to HTML</p>
