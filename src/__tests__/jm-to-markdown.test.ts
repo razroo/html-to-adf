5d69adc702fc3b40db252f10f352cf232944c026
Charlie Greenman <charlie@razroo.com>
2024-05-16 19:53:11 -0400

    ZETA-8747: Fix code for converting code blocks for jira

--- src/__tests__/jm-to-markdown.test.ts
+++ src/__tests__/jm-to-markdown.test.ts
@@ -61,7 +61,7 @@ Then the ADF description is successfully converted to HTML</p>
   });
 
   it('should convert jira markdown to html if contains code as well', async() => {
-    const jiraMarkdown = "*Feature*: Convert Jira ticket ADF description to HTML\n\nScenario: User converts Jira ticket ADF to HTML\nGiven that I have a Jira ticket ADF description\nWhen I initiate the conversion process\n  And use the importJiraTicket function\nThen the ADF description is successfully converted to HTML\n\nNotes: \n\n# Will use the function here: [https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml|https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml|smart-link] \n# Will be within the importJiraTickets function";
+    const jiraMarkdown = "{noformat}Scenario Charlie Greenman would like to apply to 20 remote Angular contract roles\n  Given It is a LinkedIn job post\n    And It is remote\n    And It is a contract job\n  When it is Easy Apply\n  Then I should apply to it\n    And should use the existing resume{noformat}\n\nNotes\n\n# For now we will only apply to roles if they are remote, a contract and angular roles.\n## Can use this URL [https://www.linkedin.com|https://www.linkedin.com]\n# If asks for a cover letter, no need to supply one(just the resume)\n## Charlie Greenman’s resume if need it [https://drive.google.com|https://drive.google.com|smart-link] ";
     const result = await convertJiraMarkdownToHtml(jiraMarkdown);
     const expected = `<p><strong>Feature</strong>: Convert Jira ticket ADF description to HTML</p>
 <p>Scenario: User converts Jira ticket ADF to HTML
