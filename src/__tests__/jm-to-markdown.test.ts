9ba075be941bbc73a1cda553e60b1d563b903c1d
Charlie Greenman <charlie@razroo.com>
2024-05-16 20:08:07 -0400

    ZETA-8747: Fix unit tests for razroo

--- src/__tests__/jm-to-markdown.test.ts
+++ src/__tests__/jm-to-markdown.test.ts
@@ -63,18 +63,27 @@ Then the ADF description is successfully converted to HTML</p>
   it('should convert jira markdown to html if contains code as well', async() => {
     const jiraMarkdown = "{noformat}Scenario Charlie Greenman would like to apply to 20 remote Angular contract roles\n  Given It is a LinkedIn job post\n    And It is remote\n    And It is a contract job\n  When it is Easy Apply\n  Then I should apply to it\n    And should use the existing resume{noformat}\n\nNotes\n\n# For now we will only apply to roles if they are remote, a contract and angular roles.\n## Can use this URL [https://www.linkedin.com|https://www.linkedin.com]\n# If asks for a cover letter, no need to supply one(just the resume)\n## Charlie Greenman’s resume if need it [https://drive.google.com|https://drive.google.com|smart-link] ";
     const result = await convertJiraMarkdownToHtml(jiraMarkdown);
-    const expected = `<p><strong>Feature</strong>: Convert Jira ticket ADF description to HTML</p>
-<p>Scenario: User converts Jira ticket ADF to HTML
-Given that I have a Jira ticket ADF description
-When I initiate the conversion process
-  And use the importJiraTicket function
-Then the ADF description is successfully converted to HTML</p>
-<p>Notes: </p>
+    const expected = `<pre><code>Scenario Charlie Greenman would like to apply to 20 remote Angular contract roles
+  Given It is a LinkedIn job post
+    And It is remote
+    And It is a contract job
+  When it is Easy Apply
+  Then I should apply to it
+    And should use the existing resume
+</code></pre>
+<p>Notes</p>
 <ol>
-<li>Will use the function here: <a href="https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml">https://github.com/razroo/html-to-adf?tab=readme-ov-file#convertadftohtml</a> </li>
-<li>Will be within the importJiraTickets function</li>
+<li>For now we will only apply to roles if they are remote, a contract and angular roles.<ol>
+<li>Can use this URL <a href="https://www.linkedin.com">https://www.linkedin.com</a></li>
+</ol>
+</li>
+<li>If asks for a cover letter, no need to supply one(just the resume)<ol>
+<li>Charlie Greenman’s resume if need it <a href="https://drive.google.com">https://drive.google.com</a></li>
+</ol>
+</li>
 </ol>
 `;
+    expect(result).toEqual(expected);
     expect(result).toEqual(expected);
   });
 })
